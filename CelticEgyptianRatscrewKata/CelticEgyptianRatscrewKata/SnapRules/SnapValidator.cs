c03400dc87da695e22fbceea2179e7cb322baaa6
Peter Gerrard <32jezza@gmail.com>
2015-01-23 14:58:31 +0000

    Create snap validator with a simple test for no rules.

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SnapRules/SnapValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace CelticEgyptianRatscrewKata.SnapRules
+{
+    /// <summary>
+    /// Aggregates snap rules to see if any are true.
+    /// </summary>
+    public class SnapValidator
+    {
+        /// <summary>
+        /// Checks if <paramref name="cardStack"/> has any valid snaps.
+        /// </summary>
+        public bool IsSnapValid(Stack cardStack)
+        {
+            return false;
+        }
+    }
+}
---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapshotValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using CelticEgyptianRatscrewKata.SnapRules;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    [TestFixture]
+    public class SnapshotValidatorTests
+    {
+        [Test]
+        public void ReturnsFalseIfNoRulesAndNoStack()
+        {
+            //ARRANGE
+            var snapValidator = new SnapValidator();
+            var emptyStack = new Stack(Enumerable.Empty<Card>());
+
+            //ACT
+            var result = snapValidator.IsSnapValid(emptyStack);
+
+            //ASSERT
+            Assert.IsFalse(result);
+        }
+    }
+}
