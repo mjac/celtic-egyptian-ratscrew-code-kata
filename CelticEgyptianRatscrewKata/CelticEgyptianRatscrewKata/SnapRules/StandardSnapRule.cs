6e5e9bab825be303dfc4a1a0a5192b2f2fa5a5d0
Peter Gerrard <32jezza@gmail.com>
2015-01-23 14:58:28 +0000

    Green: stack with two cards with equal rank.

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SnapRules/StandardSnapRule.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SnapRules/StandardSnapRule.cs
@@ -1,4 +1,6 @@
-﻿namespace CelticEgyptianRatscrewKata.SnapRules
+﻿using System.Linq;
+
+namespace CelticEgyptianRatscrewKata.SnapRules
 {
     /// <summary>
     /// Represents a standard snap, i.e. any two adjacent cards have the same rank.
@@ -7,6 +9,15 @@
     {
         public bool IsSnapValid(Stack cardStack)
         {
+            Rank? previous = null;
+            foreach (var card in cardStack)
+            {
+                if (card.Rank == previous)
+                {
+                    return true;
+                }
+                previous = card.Rank;
+            }
             return false;
         }
     }
