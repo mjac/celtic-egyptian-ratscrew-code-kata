1e43e76c176155be4d9cd185a12b516d65b4363b
Peter Gerrard <32jezza@gmail.com>
2015-01-23 14:58:34 +0000

    Passing test to ensure it is checking the correct stack.

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapshotValidatorTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapshotValidatorTests.cs
@@ -34,5 +34,27 @@ namespace CelticEgyptianRatscrewKata.Tests
             //ASSERT
             Assert.IsTrue(result);
         }
+
+        [Test]
+        public void ReturnsTrueOnlyOnSpecificStack()
+        {
+            //ARRANGE
+            var cardStack = new Stack(new []
+            {
+                new Card(Suit.Clubs, Rank.Ace) 
+            });
+
+            var alwaysTrueRule = Substitute.For<ISnapRule>();
+            alwaysTrueRule.IsSnapValid(Arg.Any<Stack>()).Returns(false);
+            alwaysTrueRule.IsSnapValid(cardStack).Returns(true);
+
+            var snapValidator = new SnapValidator(alwaysTrueRule);
+
+            //ACT
+            var result = snapValidator.IsSnapValid(cardStack);
+
+            //ASSERT
+            Assert.IsTrue(result);
+        }
     }
 }
