4e6e74c81e9251899c1cb78f944c9735b95d8023
Peter Gerrard <32jezza@gmail.com>
2015-01-23 14:58:36 +0000

    Red: Tests for Dark queen snap behaviour.

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SnapRules/DarkQueenRule.cs
@@ -0,0 +1,15 @@
+﻿using CelticEgyptianRatscrewKata.SnapRules;
+
+namespace CelticEgyptianRatscrewKata
+{
+    /// <summary>
+    /// Represents the rule where the queen of spades is on the top.
+    /// </summary>
+    public class DarkQueenRule : ISnapRule
+    {
+        public bool IsSnapValid(Stack cardStack)
+        {
+            return false;
+        }
+    }
+}
---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapRules/DarkQueenRuleTests.cs
@@ -0,0 +1,75 @@
+﻿using CelticEgyptianRatscrewKata.SnapRules;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests.SnapRules
+{
+    [TestFixture]
+    public class DarkQueenRuleTests
+    {
+        [Test]
+        public void ReturnsFalseOnEmptyStack()
+        {
+            //ARRANGE
+            var sandwichSnapRule = new DarkQueenRule();
+
+            //ACT
+            var result = sandwichSnapRule.IsSnapValid(Stack.Empty());
+
+            //ASSERT
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ReturnsTrueWhenOnlyDarkQueenInStack()
+        {
+            //ARRANGE
+            var sandwichSnapRule = new DarkQueenRule();
+            var stack = new Stack(new[]
+            {
+                new Card(Suit.Spades, Rank.Queen)
+            });
+
+            //ACT
+            var result = sandwichSnapRule.IsSnapValid(stack);
+
+            //ASSERT
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ReturnsTrueWhenDarkQueenTopOfStack()
+        {
+            //ARRANGE
+            var sandwichSnapRule = new DarkQueenRule();
+            var stack = new Stack(new[]
+            {
+                new Card(Suit.Spades, Rank.Queen),
+                new Card(Suit.Spades, Rank.Ace)
+            });
+
+            //ACT
+            var result = sandwichSnapRule.IsSnapValid(stack);
+
+            //ASSERT
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ReturnsFalseWhenDarkQueenNotTopOfStack()
+        {
+            //ARRANGE
+            var sandwichSnapRule = new DarkQueenRule();
+            var stack = new Stack(new[]
+            {
+                new Card(Suit.Spades, Rank.Ace),
+                new Card(Suit.Spades, Rank.Queen)
+            });
+
+            //ACT
+            var result = sandwichSnapRule.IsSnapValid(stack);
+
+            //ASSERT
+            Assert.IsFalse(result);
+        }
+    }
+}
