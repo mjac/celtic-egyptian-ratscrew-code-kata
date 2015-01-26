3727001a0cb1a9925539124b76c9340f6c525569
Michael Clark <michael.clark@red-gate.com>
2015-01-26 15:41:53 +0000

    Created SnapValidatorFactory class and removed old SnapValidator class.

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SnapValidatorFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata
+{
+    class SnapValidatorFactory
+    {
+        public ISnapValidator CreateSnapValidator()
+        {
+            var compositeValidator = new CompositeValidator();
+
+            compositeValidator.AddValidator(new DarkQueenValidator());
+            compositeValidator.AddValidator(new StandardValidator());
+            compositeValidator.AddValidator(new SandwichValidator());
+
+            return compositeValidator;
+        }
+    }
+}
