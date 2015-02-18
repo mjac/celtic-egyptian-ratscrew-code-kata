3a0f5f08cc745e078e6400ee6298229372a6e426
Peter Gerrard <32jezza@gmail.com>
2015-02-18 11:51:56 +0000

    Green: store and use key bindings for snaps

--- CelticEgyptianRatscrewKata/ConsoleBasedGame/ActionManager.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/ActionManager.cs
@@ -1,4 +1,6 @@
-﻿using CelticEgyptianRatscrewKata.Game;
+﻿using System;
+using System.Collections.Generic;
+using CelticEgyptianRatscrewKata.Game;
 
 namespace ConsoleBasedGame
 {
@@ -7,21 +9,27 @@ namespace ConsoleBasedGame
     /// </summary>
     internal class ActionManager
     {
+        private readonly IDictionary<char, Action> _keyBindings;
         private readonly IGameController _gameController;
 
         public ActionManager(IGameController gameController)
         {
+            _keyBindings = new Dictionary<char, Action>();
             _gameController = gameController;
         }
 
         public void Process(char input)
         {
-
+            if (_keyBindings.ContainsKey(input))
+            {
+                var action = _keyBindings[input];
+                action();
+            }
         }
 
         public void Bind(PlayerInfo playerInfo)
         {
-
+            _keyBindings.Add(playerInfo.SnapKey, () => _gameController.AttemptSnap(playerInfo));
         }
     }
 }
