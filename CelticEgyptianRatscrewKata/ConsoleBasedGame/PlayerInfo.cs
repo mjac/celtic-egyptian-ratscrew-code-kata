306ff1adaa6d677a8af9e6fda9bb4823733c88ee
Peter Gerrard <32jezza@gmail.com>
2015-02-18 11:47:38 +0000

    GREEN: Override equality members in PlayerInfo.

--- CelticEgyptianRatscrewKata/ConsoleBasedGame/PlayerInfo.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/PlayerInfo.cs
@@ -19,5 +19,32 @@ namespace ConsoleBasedGame
         {
             return string.Format("{0} - Play '{1}' - Snap '{2}'", Name, PlayCardKey, SnapKey);
         }
+
+        #region Equality Members
+
+        protected bool Equals(PlayerInfo other)
+        {
+            return string.Equals(Name, other.Name) && PlayCardKey == other.PlayCardKey && SnapKey == other.SnapKey;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((PlayerInfo)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ PlayCardKey.GetHashCode();
+                hashCode = (hashCode * 397) ^ SnapKey.GetHashCode();
+                return hashCode;
+            }
+        }
+        #endregion
     }
 }
