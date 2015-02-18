72d0cb0d2bf1177ba651493fdbcd575b3a30c464
Peter Gerrard <32jezza@gmail.com>
2015-02-18 11:33:02 +0000

    Passing test for when no keys bound.

---
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/ActionManager.cs
@@ -0,0 +1,22 @@
+﻿using CelticEgyptianRatscrewKata.Game;
+
+namespace ConsoleBasedGame
+{
+    /// <summary>
+    /// Maps key bindings to actions on an <see cref="IGameController"/>.
+    /// </summary>
+    public class ActionManager
+    {
+        private readonly IGameController _gameController;
+
+        public ActionManager(IGameController gameController)
+        {
+            _gameController = gameController;
+        }
+
+        public void Process(char input)
+        {
+
+        }
+    }
+}
---
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/Tests/ActionManagerTests.cs
@@ -0,0 +1,31 @@
+﻿using CelticEgyptianRatscrewKata.Game;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ConsoleBasedGame.Tests
+{
+    [TestFixture]
+    public class ActionManagerTests
+    {
+        private IGameController _gameController;
+        private ActionManager _actionManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _gameController = Substitute.For<IGameController>();
+            _actionManager = new ActionManager(_gameController);
+        }
+
+        [Test]
+        public void DoesNothingWhenNoKeysBound()
+        {
+            // ACT
+            _actionManager.Process('a');
+
+            // ASSERT
+            _gameController.DidNotReceive().AttemptSnap(Arg.Any<IPlayer>());
+            _gameController.DidNotReceive().PlayCard(Arg.Any<IPlayer>());
+        }
+    }
+}
