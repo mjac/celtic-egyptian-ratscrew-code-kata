e4432a4167f95f16a0ebdd0156003252c280b35f
Peter Gerrard <32jezza@gmail.com>
2015-03-09 14:41:27 +0000

    Passing end to end tests to check penalty removal.

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/EndToEndPenaltyTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/EndToEndPenaltyTests.cs
@@ -32,5 +32,55 @@ namespace CelticEgyptianRatscrewKata.Tests
             // ASSERT
             Assert.IsFalse(success, "The snap should not succeed");
         }
+
+        [Test]
+        public void PenaltyIsRemovedAfterSuccessfulSnap()
+        {
+            // ARRANGE
+            var player1 = new Player("p1");
+            var player2 = new Player("p2");
+            var snapValidator = Substitute.For<ISnapValidator>();
+            snapValidator.CanSnap(Arg.Any<Cards>()).Returns(false, true);
+
+            var gameController = new GameController(new GameState(), snapValidator, Substitute.For<IDealer>(), Substitute.For<IShuffler>());
+            gameController.AddPlayer(player1);
+            gameController.AddPlayer(player2);
+
+            // ACT
+            // Get a penalty
+            gameController.AttemptSnap(player1);
+            var snapped = gameController.AttemptSnap(player2);
+            // Try a `valid` snap, after penalty removed
+            var success = gameController.AttemptSnap(player1);
+
+            // ASSERT
+            Assert.IsTrue(snapped, "The snap should succeed, this is ensuring player 2 did not get a penalty");
+            Assert.IsTrue(success, "The snap should succeed");
+        }
+
+        [Test]
+        public void PenaltyIsRemovedWhenEveryoneHasAPenalty()
+        {
+            // ARRANGE
+            var player1 = new Player("p1");
+            var player2 = new Player("p2");
+            var snapValidator = Substitute.For<ISnapValidator>();
+            snapValidator.CanSnap(Arg.Any<Cards>()).Returns(false, false, true);
+
+            var gameController = new GameController(new GameState(), snapValidator, Substitute.For<IDealer>(), Substitute.For<IShuffler>());
+            gameController.AddPlayer(player1);
+            gameController.AddPlayer(player2);
+
+            // ACT
+            // Get a penalty
+            gameController.AttemptSnap(player1);
+            var snapped = gameController.AttemptSnap(player2);
+            // Try a `valid` snap, after penalty removed because deadlock
+            var success = gameController.AttemptSnap(player1);
+
+            // ASSERT
+            Assert.IsFalse(snapped, "The snap should not succeed, this is ensuring both players have a penalty");
+            Assert.IsTrue(success, "The snap should succeed");
+        }
     }
 }
